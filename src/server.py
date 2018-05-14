32e9db78e1472d87a78a8801f5a21553d69efc0d
Hay Kranen <huskyr@gmail.com>
2018-05-14 23:53:34 +0200

    Added some failsafe checking

--- src/server.py
+++ src/server.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 from api import parse_article, topic_for_article
 from spinque_api import ClariahSpinqueApi
 from logzero import logger
+
+# This should probably be a decorator, but i don't know how
+def check_params(*args):
+    for arg in args:
+        if arg not in request.args:
+            response = jsonify(
+                error = "Required argument not given: '%s'" % arg
+            )
+            response.status_code = 422
+            abort(response)
 
 app = Flask(__name__)
 cache = {}
@@ -41,28 +51,32 @@ def home():
 
 @app.route("/api/parse_article")
 def parse_article_():
-    url = request.args["url"]
+    check_params("url")
+    url = request.args.get("url")
     data = parse_article(url)
     return json_response(data)
 
 @app.route('/api/extract_terms')
 def extract_terms():
-    title = request.args["title"]
-    text = request.args["text"]
+    check_params("title", "text")
+    title = request.args.get("title")
+    text = request.args.get("text")
     data = spinque_api.extract_terms(title = title, text = text)
     return json_response(data)
 
 @app.route('/api/topic')
 def topic():
-    query = request.args["query"]
-    result_type = request.args["result_type"]
+    check_params("query", "result_type")
+    query = request.args.get("query")
+    result_type = request.args.get("result_type")
     data = spinque_api.topic(result_type = result_type, query = query)
     return json_response(data)
 
 @app.route("/api/topic_for_article")
 def topic_for_article_():
-    url = request.args["url"]
-    result_type = request.args["result_type"]
+    check_params("url", "result_type")
+    url = request.args.get("url")
+    result_type = request.args.get("result_type")
 
     data = topic_for_article(
         spinque_api = spinque_api,
