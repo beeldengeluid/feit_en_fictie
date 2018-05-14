2f79d52420c37375fe0594a520ab6d0f0515d938
Hay Kranen <huskyr@gmail.com>
2018-05-14 22:51:24 +0200

    Added some basic caching

--- src/server.py
+++ src/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from api import parse_article, ClariahSpinqueApi
+from logzero import logger
 
 app = Flask(__name__)
+cache = {}
 app.config.from_object('settings.Config')
 
 spinque_api = ClariahSpinqueApi(
@@ -11,6 +13,21 @@ spinque_api = ClariahSpinqueApi(
     config = app.config["SPINQUE_CONFIG_ID"]
 )
 
+# Basic cache implementation
+@app.before_request
+def check_cache():
+    if request.path in cache:
+        logger.debug("Getting '%s' from cache" % request.path)
+        return json_response(cache[request.path])
+
+def json_response(data):
+    # First save in cache if data is anything
+    if data:
+        logger.debug("Saving '%s' in cache" % request.path)
+        cache[request.path] = data
+
+    return jsonify(data)
+
 @app.route('/')
 def home():
 	return render_template('index.html',
@@ -19,22 +36,22 @@ def home():
 	   PROXY_BASE_URL = app.config['PROXY_BASE_URL']
 	)
 
-@app.route("/parse_article")
+@app.route("/api/parse_article")
 def parse_article_():
     url = request.args.get("url")
     data = parse_article(url)
-    return jsonify(data)
+    return json_response(data)
 
-@app.route('/extract_terms')
+@app.route('/api/extract_terms')
 def extract_terms():
     title = request.args["title"]
     text = request.args["text"]
     data = spinque_api.extract_terms(title = title, text = text)
-    return jsonify(data)
+    return json_response(data)
 
-@app.route('/topic')
+@app.route('/api/topic')
 def topic():
     query = request.args["query"]
     result_type = request.args["result_type"]
     data = spinque_api.topic(result_type = result_type, query = query)
-    return jsonify(data)
+    return json_response(data)
