54fd4724aad369ce33ab6e096d97ea73c9a457a5
Hay Kranen <huskyr@gmail.com>
2018-05-23 18:08:24 +0200

    Adding video player error styling

--- src/static/js/components/video-player.vue
+++ src/static/js/components/video-player.vue
@@ -1,21 +1,43 @@
 <template>
-	<video
-		v-bind:src="src"
-		controls
-	></video>
+	<div class="video-player">
+		<video
+			v-if="!error"
+			v-bind:src="src"
+			controls
+		></video>
+
+		<div
+			class="video-player__error"
+			v-if="error">
+			<h3 class="video-player__error-title">Er ging iets mis...</h3>
+			<p class="video-player__error-message">{{error}}</p>
+		</div>
+	</div>
 </template>
 
 <script>
 	export default {
 		mounted() {
+			const player = this.$el.querySelector('video');
+
+			player.addEventListener('error', (e) => {
+				this.error = e.target.error.message;
+			});
+
 			if (this.start) {
-				this.$el.addEventListener('loadedmetadata', () => {
+				player.addEventListener('loadedmetadata', () => {
 					this.$el.currentTime = this.start;
 
 					if (this.autoplay) {
-						this.$el.play();
+						player.play();
 					}
 				});
+			}
+		},
+
+		data() {
+			return {
+				error : false
 			}
 		},
 
