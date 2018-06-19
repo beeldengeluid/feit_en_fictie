1a9eea3214ffb00b0a634070970e59d842ecbfcc
Hay Kranen <huskyr@gmail.com>
2018-06-19 16:37:29 +0200

    Adding highlighting to result list

--- src/static/components/results-list.vue
+++ src/static/components/results-list.vue
@@ -8,17 +8,13 @@
                 <button
                     v-on:click="play(item.media)"
                     class="results-list__item">
-                    <h3>
-                        {{item.title}}
-                    </h3>
+                    <h3 v-html="highlight(item.title)"></h3>
 
                     <time>
                         {{item.date}}
                     </time>
 
-                    <p>
-                        {{item.description}}
-                    </p>
+                    <p v-html="highlight(item.description)"></p>
                 </button>
             </li>
         </ul>
@@ -26,14 +22,42 @@
 </template>
 
 <script>
+    import { map, words } from 'lodash';
+
     export default {
         computed: {
             results() {
                 return this.$store.getters.resultItems;
+            },
+
+            termlabels() {
+                return map(this.terms, 'term');
+            },
+
+            terms() {
+                return this.$store.getters.terms;
             }
         },
 
         methods : {
+            highlight(text) {
+                const wordlist = words(text);
+
+                wordlist.forEach((word) => {
+                    const isTerm = this.termlabels.indexOf(word.toLowerCase());
+
+                    if (isTerm !== -1) {
+                        const term = this.terms[isTerm];
+                        const prob = term.probability;
+                        const style = `background-color: rgba(255, 255, 0, ${prob})`;
+                        const html = `<mark style="${style}">${word}</mark>`;
+                        text = text.replace(word, html);
+                    }
+                });
+
+                return text;
+            },
+
             play(media) {
                 this.$emit('playmedia', media);
             }
